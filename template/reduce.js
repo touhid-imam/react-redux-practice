5f75480540da575a766ceda839b47a1700d45a40
touhid-imam <touhid.imam1337@gmail.com>
2022-08-16 01:38:57 +0600

    why reducers are called reducer

---
+++ template/reduce.js
@@ -0,0 +1,31 @@
+const actions = [
+    { type: "increment", payload: 1 },
+    { type: "increment", payload: 1 },
+    { type: "increment", payload: 1 },
+    { type: "decrement", payload: 1 },
+];
+
+
+const initState = {
+    value: 0
+}
+
+const counterReducer = (state, action) => {
+    if(action.type === 'increment'){
+        return {
+            ...state,
+            value : state.value + action.payload,
+        }
+    } else if(action.type === 'decrement'){
+        return {
+            ...state,
+            value : state.value - action.payload,
+        }
+    } else{
+        return state;
+    }
+}
+
+const finalResult = actions.reduce(counterReducer, initState);
+
+console.log(finalResult);
